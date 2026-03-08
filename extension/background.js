3ae38400276aff0ed0ec16ebbb9e2b6bde26c26f
Max Zhang <142354778+maxh119Z@users.noreply.github.com>
2026-03-07 16:23:49 -0800

    add

--- extension/background.js
+++ extension/background.js
@@ -334,6 +334,7 @@ STUDENT PROFILE:
 ${dynamicCtx}${statusCtx}
 
 Definitions: ${tryableCtx}
+Obvious commonsense or 1+1 questions should be high risk.
 
 EVALUATE:
 1. intent_category: "direct_answer"|"homework_completion"|"concept_clarification"|"brainstorming"|"editing_polishing"|"advanced_help"|"casual_chat"
